704a089a043c81dfda8877afaf9b89e8ef9d9eb3
Heiko Carstens <heiko.carstens@de.ibm.com>
2012-09-27 10:45:06 +0200

    s390/mm: use pfmf instruction to initialize storage keys
    Make use of the pfmf instruction, if available, to initialize the storage
    keys of whole 1MB frames instead of initializing every single page using
    the sske instruction.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>

--- arch/s390/kernel/setup.c
+++ arch/s390/kernel/setup.c
@@ -772,6 +772,27 @@ static void __init reserve_crashkernel(void)
 #endif
 }
 
+static void __init init_storage_keys(unsigned long start, unsigned long end)
+{
+	unsigned long boundary, function;
+
+	while (start < end) {
+		if (MACHINE_HAS_EDAT1) {
+			/* set storage keys for a 1MB frame */
+			function = 0x21000 | PAGE_DEFAULT_KEY;
+			boundary = (start + HPAGE_SIZE) & HPAGE_MASK;
+			if (boundary <= end) {
+				do {
+					start = pfmf(function, start);
+				} while (start & ~HPAGE_MASK);
+				continue;
+			}
+		}
+		page_set_storage_key(start, PAGE_DEFAULT_KEY, 0);
+		start += PAGE_SIZE;
+	}
+}
+
 static void __init setup_memory(void)
 {
         unsigned long bootmap_size;
@@ -850,9 +871,7 @@ static void __init setup_memory(void)
 		memblock_add_node(PFN_PHYS(start_chunk),
 				  PFN_PHYS(end_chunk - start_chunk), 0);
 		pfn = max(start_chunk, start_pfn);
-		for (; pfn < end_chunk; pfn++)
-			page_set_storage_key(PFN_PHYS(pfn),
-					     PAGE_DEFAULT_KEY, 0);
+		init_storage_keys(PFN_PHYS(pfn), PFN_PHYS(end_chunk));
 	}
 
 	psw_set_key(PAGE_DEFAULT_KEY);
