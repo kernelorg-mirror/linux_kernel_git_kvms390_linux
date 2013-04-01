41eba2efaf28e1f9decce8e3e7adb4bda6300514
Heiko Carstens <heiko.carstens@de.ibm.com>
2013-04-01 12:16:40 +0200

    Revert "s390/mm: avoid initializing vmemmap twice"
    This reverts commit 1395586314fcfb3819f565633991a4b526ebf21a.
    (008-vmemap-init-twice.diff)
    
    This patch was (also) supposed to fix an upstream compile error.
    Not needed anymore, since it got resolved differently.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>

--- arch/s390/mm/vmem.c
+++ arch/s390/mm/vmem.c
@@ -255,13 +255,14 @@ int __meminit vmemmap_populate(struct page *start, unsigned long nr, int node)
 		if (pte_none(*pt_dir)) {
 			unsigned long new_page;
 
-			new_page = __pa(vmemmap_alloc_block(PAGE_SIZE, node));
+			new_page =__pa(vmem_alloc_pages(0));
 			if (!new_page)
 				goto out;
 			pte_val(*pt_dir) = __pa(new_page);
 		}
 		address += PAGE_SIZE;
 	}
+	memset(start, 0, nr * sizeof(struct page));
 	ret = 0;
 out:
 	flush_tlb_kernel_range(start_addr, end_addr);
