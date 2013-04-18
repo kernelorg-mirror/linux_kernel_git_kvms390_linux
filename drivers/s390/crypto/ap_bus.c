0fc6e6014450fb8cc7c79320a364fdd0e29fad0a
Tuchscherer <tuch@tuxmaker.boeblingen.de.ibm.com>
2013-04-18 17:46:16 +0200

    s390/zcrypt: fix typo please merge with 00d7d70f6e344ae7e0e3607c883eedc4b59bf8ba
    Signed-off-by: Tuchscherer <tuch@tuxmaker.boeblingen.de.ibm.com>

--- drivers/s390/crypto/ap_bus.c
+++ drivers/s390/crypto/ap_bus.c
@@ -956,7 +956,7 @@ void ap_bus_force_rescan(void)
 {
 	/* reconfigure the AP bus rescan timer. */
 	mod_timer(&ap_config_timer, jiffies + ap_config_time * HZ);
-	/* processing a asynchonuous bus rescan */
+	/* processing a asynchronous bus rescan */
 	queue_work(ap_work_queue, &ap_config_work);
 	flush_work(&ap_config_work);
 }
