971b8278218c2dfd2a3c7e5d711af2b9ba9e0c99
Martin Schwidefsky <schwidefsky@de.ibm.com>
2013-04-29 09:34:57 +0200

    s390,kvm: rename KVM_MEMORY_SLOTS -> KVM_USER_MEM_SLOTS
    To be merged with 866f720e26183b2b68100a9cb72fca7143235a7f
    
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/kvm/kvm-s390.c
+++ arch/s390/kvm/kvm-s390.c
@@ -150,7 +150,7 @@ int kvm_dev_ioctl_check_extension(long ext)
 		r = KVM_MAX_VCPUS;
 		break;
 	case KVM_CAP_NR_MEMSLOTS:
-		r = KVM_MEMORY_SLOTS;
+		r = KVM_USER_MEM_SLOTS;
 		break;
 	case KVM_CAP_S390_COW:
 		r = MACHINE_HAS_ESOP;
