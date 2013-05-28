754e3ef70b837fa7f82cc6da7bbd354c47815b18
Martin Schwidefsky <schwidefsky@de.ibm.com>
2013-05-28 14:06:26 +0200

    Remove whitespace.
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/kvm_host.h
+++ arch/s390/include/asm/kvm_host.h
@@ -62,7 +62,7 @@ struct sca_block {
 #define CPUSTAT_MCDS       0x00000100
 #define CPUSTAT_SM         0x00000080
 #define CPUSTAT_G          0x00000008
-#define CPUSTAT_GED        0x00000004
+#define CPUSTAT_GED	   0x00000004
 #define CPUSTAT_J          0x00000002
 #define CPUSTAT_P          0x00000001
 
