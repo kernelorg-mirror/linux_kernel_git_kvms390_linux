5b591c78cd163cc4e1db67cc01657512678673b9
Michael Mueller <mimu@linux.vnet.ibm.com>
2013-06-20 15:11:39 +0200

    s390/facility: decompose test_facility()
    The patch decomposes the function test_facility() into its API
    test_facility() and its implementation __test_facility(). This
    allows to reuse the implementation with a different API.
    
    Patch is used to prepare checkin of SIE satellite code.
    
    Signed-off-by: Michael Mueller <mimu@linux.vnet.ibm.com>
    Acked-by: Cornelia Huck <cornelia.huck@de.ibm.com>
    Acked-by: Christian Borntraeger <borntraeger@de.ibm.com>

--- arch/s390/include/asm/facility.h
+++ arch/s390/include/asm/facility.h
@@ -18,14 +18,20 @@
  * That makes it easier to query facility bits with the bit number as
  * documented in the Principles of Operation.
  */
-static inline int test_facility(unsigned long nr)
+static inline int __test_facility(unsigned long nr, void *facilities)
 {
 	unsigned char *ptr;
 
 	if (nr >= MAX_FACILITY_BIT)
 		return 0;
-	ptr = (unsigned char *) &S390_lowcore.stfle_fac_list + (nr >> 3);
+	ptr = (unsigned char *) facilities + (nr >> 3);
 	return (*ptr & (0x80 >> (nr & 7))) != 0;
+}
+
+static inline int test_facility(unsigned long nr)
+{
+
+	return __test_facility(nr, &S390_lowcore.stfle_fac_list);
 }
 
 /**
