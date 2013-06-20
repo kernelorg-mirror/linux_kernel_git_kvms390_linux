1adff7cfccb179ca1f4d5b4337baec1a12c396e9
Stefan Raspl <raspl@linux.vnet.ibm.com>
2013-06-20 13:34:39 +0200

    Revert "s390/airq: simplify adapter interrupt code"
    This reverts commit 5044ca50850cf914d944933a5367719d86f24050.

--- arch/s390/pci/pci.c
+++ arch/s390/pci/pci.c
@@ -82,19 +82,10 @@ struct intr_bucket {
 
 static struct intr_bucket *bucket;
 
-/* Adapter interrupt definitions */
+/* Adapter local summary indicator */
+static u8 *zpci_irq_si;
+
 static atomic_t irq_retries = ATOMIC_INIT(0);
-
-static void zpci_irq_handler(struct airq_struct *airq);
-
-static u8 zpci_irq_si = 1;
-
-static struct airq_struct zpci_airq = {
-	.handler = zpci_irq_handler,
-	.lsi_ptr = &zpci_irq_si,
-	.lsi_mask = 0xff,
-	.isc = PCI_ISC,
-};
 
 /* I/O Map */
 static DEFINE_SPINLOCK(zpci_iomap_lock);
@@ -413,7 +404,7 @@ static struct pci_ops pci_root_ops = {
 /* store the last handled bit to implement fair scheduling of devices */
 static DEFINE_PER_CPU(unsigned long, next_sbit);
 
-static void zpci_irq_handler(struct airq_struct *airq)
+static void zpci_irq_handler(void *dont, void *need)
 {
 	unsigned long sbit, mbit, last = 0, start = __get_cpu_var(next_sbit);
 	int rescan = 0, max = aisb_max;
@@ -724,18 +715,25 @@ static int __init zpci_irq_init(void)
 		goto out_alloc;
 	}
 
-	rc = register_adapter_interrupt(&zpci_airq);
-	if (rc)
+	isc_register(PCI_ISC);
+	zpci_irq_si = s390_register_adapter_interrupt(&zpci_irq_handler, NULL, PCI_ISC);
+	if (IS_ERR(zpci_irq_si)) {
+		rc = PTR_ERR(zpci_irq_si);
+		zpci_irq_si = NULL;
 		goto out_ai;
+	}
 
 	for_each_online_cpu(cpu)
 		per_cpu(next_sbit, cpu) = 0;
 
 	spin_lock_init(&bucket->lock);
+	/* set summary to 1 to be called every time for the ISC */
+	*zpci_irq_si = 1;
 	set_irq_ctrl(SIC_IRQ_MODE_SINGLE, NULL, PCI_ISC);
 	return 0;
 
 out_ai:
+	isc_unregister(PCI_ISC);
 	free_page((unsigned long) bucket->alloc);
 out_alloc:
 	free_page((unsigned long) bucket->aisb);
@@ -748,7 +746,8 @@ static void zpci_irq_exit(void)
 {
 	free_page((unsigned long) bucket->alloc);
 	free_page((unsigned long) bucket->aisb);
-	unregister_adapter_interrupt(&zpci_airq);
+	s390_unregister_adapter_interrupt(zpci_irq_si, PCI_ISC);
+	isc_unregister(PCI_ISC);
 	kfree(bucket);
 }
 
