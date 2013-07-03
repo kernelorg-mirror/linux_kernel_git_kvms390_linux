9bc6bcc22a61f7a229983e4e0a3bcc0ee9c4c5cd
Martin Schwidefsky <schwidefsky@de.ibm.com>
2013-07-03 10:29:51 +0200

    mm: add mm argument to lazy mmu mode hooks, update
    Add mm argument to lazy mmu mode hooks define in arch/sparc/.
    To be merged with git commit 8051266b8dec812a02e4e3217f354d69b26e3132
    "Add mm argument to lazy mmu mode hooks".
    
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/sparc/mm/tlb.c
+++ arch/sparc/mm/tlb.c
@@ -51,14 +51,14 @@ out:
 	put_cpu_var(tlb_batch);
 }
 
-void arch_enter_lazy_mmu_mode(void)
+void arch_enter_lazy_mmu_mode(struct mm_struct *mm)
 {
 	struct tlb_batch *tb = &__get_cpu_var(tlb_batch);
 
 	tb->active = 1;
 }
 
-void arch_leave_lazy_mmu_mode(void)
+void arch_leave_lazy_mmu_mode(struct mm_struct *mm)
 {
 	struct tlb_batch *tb = &__get_cpu_var(tlb_batch);
 
