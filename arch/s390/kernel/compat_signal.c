66d3e8af0bddb1f7156e8fb798d5b3e2a2ce3f38
Heiko Carstens <heiko.carstens@de.ibm.com>
2013-09-07 11:26:29 +0200

    s390/compat_linux: fixup whitespace damage
    ...merge with
    "s390/compat signal: add couple of __force annotations"
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>

--- arch/s390/kernel/compat_signal.c
+++ arch/s390/kernel/compat_signal.c
@@ -334,7 +334,7 @@ static int setup_frame32(int sig, struct k_sigaction *ka,
 	if (ka->sa.sa_flags & SA_RESTORER) {
 		regs->gprs[14] = (__u64 __force) ka->sa.sa_restorer | PSW32_ADDR_AMODE;
 	} else {
-		regs->gprs[14] = (__u64 __force ) frame->retcode | PSW32_ADDR_AMODE;
+		regs->gprs[14] = (__u64 __force) frame->retcode | PSW32_ADDR_AMODE;
 		if (__put_user(S390_SYSCALL_OPCODE | __NR_sigreturn,
 			       (u16 __force __user *)(frame->retcode)))
 			goto give_sigsegv;
