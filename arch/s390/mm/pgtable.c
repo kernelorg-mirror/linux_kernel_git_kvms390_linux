ea13d481bab034f8c334fc51d8984e7a9aadf664
Martin Schwidefsky <schwidefsky@de.ibm.com>
2013-11-04 12:47:45 +0100

    s390/mm,tlb: tlb flush on page table upgrade fixup #2
    The crst_table_downgrade function is call for fork and execve.
    Only for execve the mm is attached to the CPU, the current user
    asce may not be replaced for fork.
    
    To be merged with commit 68f7b09dbc6efa87d31d7fe324efcf0944ac292e
    "s390/mm,tlb: correct tlb flush on page table upgrade"
    
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/mm/pgtable.c
+++ arch/s390/mm/pgtable.c
@@ -107,8 +107,10 @@ void crst_table_downgrade(struct mm_struct *mm, unsigned long limit)
 {
 	pgd_t *pgd;
 
-	clear_user_asce(mm);
-	__tlb_flush_mm(mm);
+	if (current->active_mm == mm) {
+		clear_user_asce(mm);
+		__tlb_flush_mm(mm);
+	}
 	while (mm->context.asce_limit > limit) {
 		pgd = mm->pgd;
 		switch (pgd_val(*pgd) & _REGION_ENTRY_TYPE_MASK) {
@@ -131,7 +133,8 @@ void crst_table_downgrade(struct mm_struct *mm, unsigned long limit)
 		mm->task_size = mm->context.asce_limit;
 		crst_table_free(mm, (unsigned long *) pgd);
 	}
-	update_user_asce(mm);
+	if (current->active_mm == mm)
+		update_user_asce(mm);
 }
 #endif
 
