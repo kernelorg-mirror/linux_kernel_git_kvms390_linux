7c6c63980cbcde34210d0ffcce1cdf0e174dd372
Philipp Hachtmann <phacht@linux.vnet.ibm.com>
2014-01-27 14:36:42 +0100

    mm: Get bootmem and nobootmem in sync with v3.13
    Signed-off-by: Philipp Hachtmann <phacht@linux.vnet.ibm.com>

--- mm/nobootmem.c
+++ mm/nobootmem.c
@@ -45,9 +45,7 @@ static void * __init __alloc_memory_core_early(int nid, u64 size, u64 align,
 	if (!addr)
 		return NULL;
 
-	if (memblock_reserve(addr, size))
-		return NULL;
-
+	memblock_reserve(addr, size);
 	ptr = phys_to_virt(addr);
 	memset(ptr, 0, size);
 	/*
@@ -122,18 +120,10 @@ static unsigned long __init free_low_memory_core_early(void)
 	for_each_free_mem_range(i, MAX_NUMNODES, &start, &end, NULL)
 		count += __free_memory_core(start, end);
 
-	/* Free memblock.reserved array if it was allocated */
+	/* free range that is used for reserved array if we allocate it */
 	size = get_allocated_memblock_reserved_regions_info(&start);
 	if (size)
 		count += __free_memory_core(start, start + size);
-
-#ifdef CONFIG_ARCH_DISCARD_MEMBLOCK
-
-	/* Free memblock.memory array if it was allocated */
-	size = get_allocated_memblock_memory_regions_info(&start);
-	if (size)
-		count += __free_memory_core(start, start + size);
-#endif
 
 	return count;
 }
