a294245d1aa67173f5ab117a1c0885d9164aaf2a
Ingo Tuchscherer <ingo.tuchscherer@de.ibm.com>
2014-09-22 12:55:30 +0200

    s390/zcrypt: Offset fix for test_bit routine in case of higher domain
    please merge with 0e92e1c7e68960f015dd34389abf6908490ee22e
    
    Signed-off-by: Ingo Tuchscherer <ingo.tuchscherer@de.ibm.com>

--- drivers/s390/crypto/ap_bus.c
+++ drivers/s390/crypto/ap_bus.c
@@ -1122,7 +1122,7 @@ static inline int ap_test_config(unsigned int *field, unsigned int nr)
 {
 	if (nr > 0xFFu)
 		return 0;
-	return ap_test_bit((field + (nr >> 5)), nr%32);
+	return ap_test_bit((field + (nr >> 5)), (nr & 0x1f));
 }
 
 /*
