e7aea60536a95dbcc9f8ba5540a15a95838d2f9a
Martin Schwidefsky <schwidefsky@de.ibm.com>
2014-09-26 09:37:41 +0200

    s390/topology: readd code cleanup for topology_init
    To be merged with git commit 32e7782dd827739c95c225e22aa52bf50565b201
    "s390/topology: call set_sched_topology early"
    
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/kernel/topology.c
+++ arch/s390/kernel/topology.c
@@ -464,12 +464,10 @@ static struct sched_domain_topology_level s390_topology[] = {
 
 static int __init topology_init(void)
 {
-	if (!MACHINE_HAS_TOPOLOGY) {
+	if (MACHINE_HAS_TOPOLOGY)
+		set_topology_timer();
+	else
 		topology_update_polarization_simple();
-		goto out;
-	}
-	set_topology_timer();
-out:
 	return device_create_file(cpu_subsys.dev_root, &dev_attr_dispatching);
 }
 device_initcall(topology_init);
