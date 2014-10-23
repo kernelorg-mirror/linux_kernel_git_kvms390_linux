994a096542116bc244e92a360e17c1b2c3a5c875
Dominik Dingel <dingel@linux.vnet.ibm.com>
2014-10-23 12:30:34 +0200

    Revert "s390/mm: fix compile breakage"
    This reverts commit 95ce72d33f53aeee2d60ed0a5d2a85ece5307b11.

--- arch/s390/mm/pgtable.c
+++ arch/s390/mm/pgtable.c
@@ -1065,10 +1065,9 @@ static inline unsigned long *page_table_alloc_pgste(struct mm_struct *mm,
 	return NULL;
 }
 
-int walk_pgste(struct mm_struct *mm, unsigned long start, unsigned long end,
-	       bool init_skey)
+void walk_pgste(struct mm_struct *mm, unsigned long start, unsigned long end,
+		bool init_skey)
 {
-	return 0;
 }
 
 static inline void page_table_free_pgste(unsigned long *table)
