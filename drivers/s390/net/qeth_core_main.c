312a274dc921e8866ed546f918600063d8749731
Thomas Richter <tmricht@linux.vnet.ibm.com>
2014-11-26 10:34:19 +0100

    Fix locking warning during qeth device setup
    The device driver has plenty of command buffers available
    per channel for channel command communication.
    There is no need to always wait for buffer.
    
    This fix requests a buffer and only enters the wait loop
    in the extremely rare case no buffer is available.
    
    Signed-off-by: Thomas Richter <tmricht@linux.vnet.ibm.com>

--- drivers/s390/net/qeth_core_main.c
+++ drivers/s390/net/qeth_core_main.c
@@ -789,8 +789,10 @@ static struct qeth_cmd_buffer *qeth_get_buffer(struct qeth_channel *channel)
 
 struct qeth_cmd_buffer *qeth_wait_for_buffer(struct qeth_channel *channel)
 {
-	struct qeth_cmd_buffer *buffer;
-	wait_event(channel->wait_q,
+	struct qeth_cmd_buffer *buffer = qeth_get_buffer(channel);
+
+	if (!buffer)
+		wait_event(channel->wait_q,
 		   ((buffer = qeth_get_buffer(channel)) != NULL));
 	return buffer;
 }
