c22fd53da1968f7bad30a123c41e70385299beb4
Heiko Carstens <heiko.carstens@de.ibm.com>
2015-01-05 09:53:39 +0100

    s390/timex: rename STORE_CLOCK_SIZE to STORE_CLOCK_EXT_SIZE
    Rename the STORE_CLOCK_SIZE define since it is about store clock
    extended and not store clock, which only writes eight bytes.
    
    Please merge with "s390/timex: fix get_tod_clock_ext() inline assembly".
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>

--- arch/s390/include/asm/timex.h
+++ arch/s390/include/asm/timex.h
@@ -68,20 +68,20 @@ static inline void local_tick_enable(unsigned long long comp)
 }
 
 #define CLOCK_TICK_RATE		1193180 /* Underlying HZ */
-#define STORE_CLOCK_SIZE	16	/* number of bytes store clock writes */
+#define STORE_CLOCK_EXT_SIZE	16	/* stcke writes 16 bytes */
 
 typedef unsigned long long cycles_t;
 
 static inline void get_tod_clock_ext(char *clk)
 {
-	typedef struct { char _[STORE_CLOCK_SIZE]; } addrtype;
+	typedef struct { char _[STORE_CLOCK_EXT_SIZE]; } addrtype;
 
 	asm volatile("stcke %0" : "=Q" (*(addrtype *) clk) : : "cc");
 }
 
 static inline unsigned long long get_tod_clock(void)
 {
-	unsigned char clk[STORE_CLOCK_SIZE];
+	unsigned char clk[STORE_CLOCK_EXT_SIZE];
 
 	get_tod_clock_ext(clk);
 	return *((unsigned long long *)&clk[1]);
