b3611137b131dff71262898b8f29581d70bcfeb2
Hendrik Brueckner <brueckner@linux.vnet.ibm.com>
2015-03-13 15:12:25 +0100

    perf: return ENOENT instead of ENOTSUPP
    The ENOTSUPP (which actually should be EOPNOTSUPP for user space) does not
    trigger a fallback event selection, for example, by perf record.
    If hardware support for the cycles perf event is available, but the hardware
    does not provide interrupts, returning ENOTSUPP causes perf to end.  Returning
    ENOENT causes the perf tool to fallback to a software-based cycle PMU that
    supports interrupts.
    
    The commit 53b25335dd ("perf: Disable sampled events if no PMU interrupt")
    introduced that incompatible change.
    
    Reported-by: Michael Holzheu <holzheu@linux.vnet.ibm.com>
    Signed-off-by: Hendrik Brueckner <brueckner@linux.vnet.ibm.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -7547,7 +7547,7 @@ SYSCALL_DEFINE5(perf_event_open,
 
 	if (is_sampling_event(event)) {
 		if (event->pmu->capabilities & PERF_PMU_CAP_NO_INTERRUPT) {
-			err = -ENOTSUPP;
+			err = -ENOENT;
 			goto err_alloc;
 		}
 	}
