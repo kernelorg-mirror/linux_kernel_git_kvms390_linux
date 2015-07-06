eb5dbbdfc0fb57d2d09556aeb921b383b85e57aa
Heiko Carstens <heiko.carstens@de.ibm.com>
2015-07-06 15:02:37 +0200

    s390/process: fix sfpc inline assembly
    The sfpc inline assembly within execve_tail() may incorrectly set bits
    28-31 of the sfpc instruction to a value which is not zero.
    These bits however are currently unused and therefore should be zero
    so we won't get surprised if these bits will be used in the future.
    
    Therefore remove the second operand from the inline assembly.
    
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>

--- arch/s390/kernel/process.c
+++ arch/s390/kernel/process.c
@@ -188,7 +188,7 @@ int copy_thread(unsigned long clone_flags, unsigned long new_stackp,
 asmlinkage void execve_tail(void)
 {
 	current->thread.fpu.fpc = 0;
-	asm volatile("sfpc %0,%0" : : "d" (0));
+	asm volatile("sfpc %0" : : "d" (0));
 }
 
 /*
