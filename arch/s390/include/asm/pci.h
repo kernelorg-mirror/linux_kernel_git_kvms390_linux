0bfef80b2fca4f83fa0235a2b12a0565b4f8b332
Gerald Schaefer <gerald.schaefer@de.ibm.com>
2015-08-27 15:33:03 +0200

    iommu/s390: Add iommu api for s390 pci devices
    This adds an IOMMU API implementation for s390 PCI devices.
    
    Reviewed-by: Sebastian Ott <sebott@linux.vnet.ibm.com>
    Signed-off-by: Gerald Schaefer <gerald.schaefer@de.ibm.com>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- arch/s390/include/asm/pci.h
+++ arch/s390/include/asm/pci.h
@@ -84,6 +84,8 @@ struct zpci_bar_struct {
 	u8		size;		/* order 2 exponent */
 };
 
+struct s390_domain;
+
 /* Private data per function */
 struct zpci_dev {
 	struct pci_dev	*pdev;
@@ -140,6 +142,8 @@ struct zpci_dev {
 
 	struct dentry	*debugfs_dev;
 	struct dentry	*debugfs_perf;
+
+	struct s390_domain *s390_domain; /* s390 IOMMU domain data */
 };
 
 static inline bool zdev_enabled(struct zpci_dev *zdev)
