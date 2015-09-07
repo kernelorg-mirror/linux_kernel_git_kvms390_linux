54316c5c03e2cd67a1106624067641f4919a1159
Hendrik Brueckner <brueckner@linux.vnet.ibm.com>
2015-09-07 11:08:00 +0200

    Revert "perf: correct event accounting imbalance on error path"
    This reverts commit 3fcee1a0fa4fcd18ebc16a811d90f9c06050b8be.
    
    I will rework this.

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -8020,8 +8020,7 @@ SYSCALL_DEFINE5(perf_event_open,
 	if (is_sampling_event(event)) {
 		if (event->pmu->capabilities & PERF_PMU_CAP_NO_INTERRUPT) {
 			err = -ENOENT;
-			__free_event(event);
-			goto err_cpus;
+			goto err_alloc;
 		}
 	}
 
