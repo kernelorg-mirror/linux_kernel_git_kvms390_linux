5402ea6af11dc5a976c5a4b11ba6fbf4f7351d42
Martin Schwidefsky <schwidefsky@de.ibm.com>
2015-09-28 15:27:26 +0200

    s390/barrier: remove unnecessary serialization in atomics and bitops
    The principles of operation states reads are in order, writes are in
    order, writes can be reordered after reads, but no reads can be
    reordered after writes.
    
    The atomic and bitops variantes for z196 use the interlocked-access
    facility instructions with a memory barrier before and after the
    instruction. Because of the memory ordering the first barrier is
    unnecessary and can be removed.
    
    Acked-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/atomic.h
+++ arch/s390/include/asm/atomic.h
@@ -35,7 +35,6 @@
 									\
 	typecheck(atomic_t *, ptr);					\
 	asm volatile(							\
-		__barrier						\
 		op_string "	%0,%2,%1\n"				\
 		__barrier						\
 		: "=d" (old_val), "+Q" ((ptr)->counter)			\
@@ -175,7 +174,6 @@ static inline int __atomic_add_unless(atomic_t *v, int a, int u)
 									\
 	typecheck(atomic64_t *, ptr);					\
 	asm volatile(							\
-		__barrier						\
 		op_string "	%0,%2,%1\n"				\
 		__barrier						\
 		: "=d" (old_val), "+Q" ((ptr)->counter)			\
