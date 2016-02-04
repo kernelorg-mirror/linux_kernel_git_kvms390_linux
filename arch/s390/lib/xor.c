9b6945297ea9a2186fb01cbd58b88f14ef4bd6d8
Martin Schwidefsky <schwidefsky@de.ibm.com>
2016-02-04 10:45:28 +0100

    s390/xor: optimized xor routing using the XC instruction [update #2]
    Heiko prefers the variant with proper C functions containing an asm
    inline.
    
    To be merged with git commit 0706cb24dd66124630a358012a7b168d0d3b7f80
    
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/lib/xor.c
+++ arch/s390/lib/xor.c
@@ -8,127 +8,121 @@
 #include <linux/types.h>
 #include <linux/module.h>
 
-void xor_xc_2(unsigned long, unsigned long *, unsigned long *);
-asm(
-	"	.globl xor_xc_2\n"
-	"	.align	4, 0x07\n"
-	"xor_xc_2:\n"
-	"	larl	%r1,2f\n"
-	"	aghi	%r2,-1\n"
-	"	bmr	%r14\n"
-	"	srlg	%r0,%r2,8\n"
-	"	ltgr	%r0,%r0\n"
-	"	jz	1f\n"
-	"0:	xc	0(256,%r3),0(%r4)\n"
-	"	la	%r3,256(%r3)\n"
-	"	la	%r4,256(%r4)\n"
-	"	brctg	%r0,0b\n"
-	"1:	ex	%r2,0(%r1)\n"
-	"	br	%r14\n"
-	"2:	xc	0(1,%r3),0(%r4)\n"
-	"	.size xor_xc_2, . - xor_xc_2\n"
-	"	.type xor_xc_2, @function\n");
-
+void xor_xc_2(unsigned long bytes, unsigned long *p1, unsigned long *p2)
+{
+	asm volatile(
+		"	larl	1,2f\n"
+		"	aghi	%0,-1\n"
+		"	jm	3f\n"
+		"	srlg	0,%0,8\n"
+		"	ltgr	0,0\n"
+		"	jz	1f\n"
+		"0:	xc	0(256,%1),0(%2)\n"
+		"	la	%1,256(%1)\n"
+		"	la	%2,256(%2)\n"
+		"	brctg	0,0b\n"
+		"1:	ex	%0,0(1)\n"
+		"	j	3f\n"
+		"2:	xc	0(1,%1),0(%2)\n"
+		"3:\n"
+		: : "d" (bytes), "a" (p1), "a" (p2)
+		: "0", "1", "cc", "memory");
+}
 EXPORT_SYMBOL(xor_xc_2);
 
-void xor_xc_3(unsigned long, unsigned long *, unsigned long *,
-	      unsigned long *);
-asm(
-	"	.globl xor_xc_3\n"
-	"	.align	4, 0x07\n"
-	"xor_xc_3:\n"
-	"	larl	%r1,2f\n"
-	"	aghi	%r2,-1\n"
-	"	bmr	%r14\n"
-	"	srlg	%r0,%r2,8\n"
-	"	ltgr	%r0,%r0\n"
-	"	jz	1f\n"
-	"0:	xc	0(256,%r3),0(%r4)\n"
-	"	xc	0(256,%r3),0(%r5)\n"
-	"	la	%r3,256(%r3)\n"
-	"	la	%r4,256(%r4)\n"
-	"	la	%r5,256(%r5)\n"
-	"	brctg	%r0,0b\n"
-	"1:	ex	%r2,0(%r1)\n"
-	"	ex	%r2,6(%r1)\n"
-	"	br	%r14\n"
-	"2:	xc	0(1,%r3),0(%r4)\n"
-	"	xc	0(1,%r3),0(%r5)\n"
-	"	.size xor_xc_3, . - xor_xc_3\n"
-	"	.type xor_xc_3, @function\n");
-
+void xor_xc_3(unsigned long bytes, unsigned long *p1, unsigned long *p2,
+	      unsigned long *p3)
+{
+	asm volatile(
+		"	larl	1,2f\n"
+		"	aghi	%0,-1\n"
+		"	jm	3f\n"
+		"	srlg	0,%0,8\n"
+		"	ltgr	0,0\n"
+		"	jz	1f\n"
+		"0:	xc	0(256,%1),0(%2)\n"
+		"	xc	0(256,%1),0(%3)\n"
+		"	la	%1,256(%1)\n"
+		"	la	%2,256(%2)\n"
+		"	la	%3,256(%3)\n"
+		"	brctg	0,0b\n"
+		"1:	ex	%0,0(1)\n"
+		"	ex	%0,6(1)\n"
+		"	j	3f\n"
+		"2:	xc	0(1,%1),0(%2)\n"
+		"	xc	0(1,%1),0(%3)\n"
+		"3:\n"
+		: "+d" (bytes), "+a" (p1), "+a" (p2), "+a" (p3)
+		: : "0", "1", "cc", "memory");
+}
 EXPORT_SYMBOL(xor_xc_3);
 
-void xor_xc_4(unsigned long, unsigned long *, unsigned long *,
-	      unsigned long *, unsigned long *);
-asm(
-	"	.globl xor_xc_4\n"
-	"	.align	4, 0x07\n"
-	"xor_xc_4:\n"
-	"	stg	%r6,72(%r15)\n"
-	"	larl	%r1,2f\n"
-	"	aghi	%r2,-1\n"
-	"	bmr	%r14\n"
-	"	srlg	%r0,%r2,8\n"
-	"	ltgr	%r0,%r0\n"
-	"	jz	1f\n"
-	"0:	xc	0(256,%r3),0(%r4)\n"
-	"	xc	0(256,%r3),0(%r5)\n"
-	"	xc	0(256,%r3),0(%r6)\n"
-	"	la	%r3,256(%r3)\n"
-	"	la	%r4,256(%r4)\n"
-	"	la	%r5,256(%r5)\n"
-	"	la	%r6,256(%r6)\n"
-	"	brctg	%r0,0b\n"
-	"1:	ex	%r2,0(%r1)\n"
-	"	ex	%r2,6(%r1)\n"
-	"	ex	%r2,12(%r1)\n"
-	"	ex	%r2,18(%r1)\n"
-	"	lg	%r6,72(%r15)\n"
-	"	br	%r14\n"
-	"2:	xc	0(1,%r3),0(%r4)\n"
-	"	xc	0(1,%r3),0(%r5)\n"
-	"	xc	0(1,%r3),0(%r6)\n"
-	"	.size xor_xc_4, . - xor_xc_4\n"
-	"	.type xor_xc_4, @function\n");
-
+void xor_xc_4(unsigned long bytes, unsigned long *p1, unsigned long *p2,
+	      unsigned long *p3, unsigned long *p4)
+{
+	asm volatile(
+		"	larl	1,2f\n"
+		"	aghi	%0,-1\n"
+		"	jm	3f\n"
+		"	srlg	0,%0,8\n"
+		"	ltgr	0,0\n"
+		"	jz	1f\n"
+		"0:	xc	0(256,%1),0(%2)\n"
+		"	xc	0(256,%1),0(%3)\n"
+		"	xc	0(256,%1),0(%4)\n"
+		"	la	%1,256(%1)\n"
+		"	la	%2,256(%2)\n"
+		"	la	%3,256(%3)\n"
+		"	la	%4,256(%4)\n"
+		"	brctg	0,0b\n"
+		"1:	ex	%0,0(1)\n"
+		"	ex	%0,6(1)\n"
+		"	ex	%0,12(1)\n"
+		"	j	3f\n"
+		"2:	xc	0(1,%1),0(%2)\n"
+		"	xc	0(1,%1),0(%3)\n"
+		"	xc	0(1,%1),0(%4)\n"
+		"3:\n"
+		: "+d" (bytes), "+a" (p1), "+a" (p2), "+a" (p3), "+a" (p4)
+		: : "0", "1", "cc", "memory");
+}
 EXPORT_SYMBOL(xor_xc_4);
 
-void xor_xc_5(unsigned long, unsigned long *, unsigned long *,
-	      unsigned long *, unsigned long *, unsigned long *);
-asm(
-	"	.globl xor_xc_5\n"
-	"	.align	4, 0x07\n"
-	"xor_xc_5:\n"
-	"	stmg	%r6,%r7,72(%r15)\n"
-	"	larl	%r1,2f\n"
-	"	aghi	%r2,-1\n"
-	"	bmr	%r14\n"
-	"	lg	%r7,160(%r15)\n"
-	"	srlg	%r0,%r2,8\n"
-	"	ltgr	%r0,%r0\n"
-	"	jz	1f\n"
-	"0:	xc	0(256,%r3),0(%r4)\n"
-	"	xc	0(256,%r3),0(%r5)\n"
-	"	xc	0(256,%r3),0(%r6)\n"
-	"	xc	0(256,%r3),0(%r7)\n"
-	"	la	%r3,256(%r3)\n"
-	"	la	%r4,256(%r4)\n"
-	"	la	%r5,256(%r5)\n"
-	"	la	%r6,256(%r6)\n"
-	"	la	%r7,256(%r7)\n"
-	"	brctg	%r0,0b\n"
-	"1:	ex	%r2,0(%r1)\n"
-	"	ex	%r2,6(%r1)\n"
-	"	ex	%r2,12(%r1)\n"
-	"	ex	%r2,18(%r1)\n"
-	"	lmg	%r6,%r7,72(%r15)\n"
-	"	br	%r14\n"
-	"2:	xc	0(1,%r3),0(%r4)\n"
-	"	xc	0(1,%r3),0(%r5)\n"
-	"	xc	0(1,%r3),0(%r6)\n"
-	"	xc	0(1,%r3),0(%r7)\n"
-	"	.size xor_xc_5, . - xor_xc_5\n"
-	"	.type xor_xc_5, @function\n");
+void xor_xc_5(unsigned long bytes, unsigned long *p1, unsigned long *p2,
+	      unsigned long *p3, unsigned long *p4, unsigned long *p5)
+{
+	/* Get around a gcc oddity */
+	register unsigned long *reg7 asm ("7") = p5;
 
+	asm volatile(
+		"	larl	1,2f\n"
+		"	aghi	%0,-1\n"
+		"	jm	3f\n"
+		"	srlg	0,%0,8\n"
+		"	ltgr	0,0\n"
+		"	jz	1f\n"
+		"0:	xc	0(256,%1),0(%2)\n"
+		"	xc	0(256,%1),0(%3)\n"
+		"	xc	0(256,%1),0(%4)\n"
+		"	xc	0(256,%1),0(%5)\n"
+		"	la	%1,256(%1)\n"
+		"	la	%2,256(%2)\n"
+		"	la	%3,256(%3)\n"
+		"	la	%4,256(%4)\n"
+		"	la	%5,256(%5)\n"
+		"	brctg	0,0b\n"
+		"1:	ex	%0,0(1)\n"
+		"	ex	%0,6(1)\n"
+		"	ex	%0,12(1)\n"
+		"	ex	%0,18(1)\n"
+		"	j	3f\n"
+		"2:	xc	0(1,%1),0(%2)\n"
+		"	xc	0(1,%1),0(%3)\n"
+		"	xc	0(1,%1),0(%4)\n"
+		"	xc	0(1,%1),0(%5)\n"
+		"3:\n"
+		: "+d" (bytes), "+a" (p1), "+a" (p2), "+a" (p3), "+a" (p4),
+		  "+a" (reg7)
+		: : "0", "1", "cc", "memory");
+}
 EXPORT_SYMBOL(xor_xc_5);
