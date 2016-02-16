7ac569ff4af896336a51ed9d2a6f5d5f02183044
Martin Schwidefsky <schwidefsky@de.ibm.com>
2016-02-16 13:32:57 +0100

    s390/mm: four page table levels vs. fork [update]
    Forking a 31-bit process now creates a 2 level page table directly.
    This has an impact on the counting of the number of pmds. An
    additional mm_inc_nr_pmds() in init_new_context() is required.
    
    To be merged with git commit d662caed5288acfc656b4f6ad49430c91c514b70
    "s390/mm: four page table levels vs. fork"
    
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/mmu_context.h
+++ arch/s390/include/asm/mmu_context.h
@@ -28,6 +28,8 @@ static inline int init_new_context(struct task_struct *tsk,
 		mm->context.asce_bits = _ASCE_TABLE_LENGTH |
 			_ASCE_USER_BITS | _ASCE_TYPE_REGION3;
 		mm->context.asce_limit = STACK_TOP_MAX;
+	} else if (mm->context.asce_limit == (1UL << 31)) {
+		mm_inc_nr_pmds(mm);
 	}
 	crst_table_init((unsigned long *) mm->pgd, pgd_entry_type(mm));
 	return 0;
