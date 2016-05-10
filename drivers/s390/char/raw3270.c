04f6e86a23c80988931905cd1be5ffe443b12343
Martin Schwidefsky <schwidefsky@de.ibm.com>
2016-05-10 12:56:21 +0200

    s390/3270: get size detection with writesf_readpart working again
    On LPAR the size detection requires an write-structured-field with
    the 3270 read-partition command and a read-modified to get the
    result. Git commit 85eb0c048d189a841f1ed2475f47f1d81a0d6496
    "s390/3270: handle reconnect of a tty with a different size"
    resets the device state to RAW3270_STATE_INIT but did not cancel
    queued requests. This breaks the size detection.
    
    To solve this cancel all queued requests if the terinal is
    disconnected and start from scratch.
    
    To be merged with git commit 85eb0c048d189a84.
    
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- drivers/s390/char/raw3270.c
+++ drivers/s390/char/raw3270.c
@@ -90,7 +90,7 @@ module_param(tubxcorrect, bool, 0);
  */
 DECLARE_WAIT_QUEUE_HEAD(raw3270_wait_queue);
 
-static int __raw3270_reset_device(struct raw3270 *rp);
+static void __raw3270_disconnect(struct raw3270 *rp);
 
 /*
  * Encode array for 12 bit 3270 addresses.
@@ -340,10 +340,8 @@ raw3270_irq (struct ccw_device *cdev, unsigned long intparm, struct irb *irb)
 		if ((irb->scsw.cmd.dstat & DEV_STAT_UNIT_CHECK) &&
 		    (irb->ecw[0] & SNS0_INTERVENTION_REQ)) {
 			set_bit(RAW3270_FLAGS_BUSY, &rp->flags);
-			if (rp->state > RAW3270_STATE_RESET) {
-				rp->state = RAW3270_STATE_INIT;
-				__raw3270_reset_device(rp);
-			}
+			if (rp->state > RAW3270_STATE_RESET)
+				__raw3270_disconnect(rp);
 		}
 		/* Call interrupt handler of the view */
 		if (view)
@@ -354,8 +352,7 @@ raw3270_irq (struct ccw_device *cdev, unsigned long intparm, struct irb *irb)
 		/* Device busy, do not start I/O */
 		return;
 
-	if (rq) {
-		BUG_ON(list_empty(&rq->list));
+	if (rq && !list_empty(&rq->list)) {
 		/* The request completed, remove from queue and do callback. */
 		list_del_init(&rq->list);
 		if (rq->callback)
@@ -639,6 +636,28 @@ raw3270_reset(struct raw3270_view *view)
 	else
 		rc = raw3270_reset_device(view->dev);
 	return rc;
+}
+
+static void
+__raw3270_disconnect(struct raw3270 *rp)
+{
+	struct raw3270_request *rq;
+	struct raw3270_view *view;
+
+	rp->state = RAW3270_STATE_INIT;
+	rp->view = &rp->init_view;
+	/* Cancel all queued requests */
+	while (!list_empty(&rp->req_queue)) {
+		rq = list_entry(rp->req_queue.next,struct raw3270_request,list);
+		view = rq->view;
+		rq->rc = -EACCES;
+		list_del_init(&rq->list);
+		if (rq->callback)
+			rq->callback(rq, rq->callback_data);
+		raw3270_put_view(view);
+	}
+	/* Start from scratch */
+	__raw3270_reset_device(rp);
 }
 
 static void
