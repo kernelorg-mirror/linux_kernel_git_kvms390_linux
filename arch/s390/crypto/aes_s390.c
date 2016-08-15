b410bf5874b6de9cb53cf68d2085a9412ed72ffc
Martin Schwidefsky <schwidefsky@de.ibm.com>
2016-08-15 14:41:12 +0200

    s390/crypto: use on-stack fallback descriptor
    In case the software fallback needs to be used, do not replace the
    tfm pointer in the passed descriptor. Instead create a new descriptor
    on the stack with a tfm pointer to the software algorithm.
    
    Reviewed-by: Harald Freudenberger <freude@linux.vnet.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/crypto/aes_s390.c
+++ arch/s390/crypto/aes_s390.c
@@ -194,34 +194,28 @@ static int fallback_blk_dec(struct blkcipher_desc *desc,
 		struct scatterlist *dst, struct scatterlist *src,
 		unsigned int nbytes)
 {
-	unsigned int ret;
-	struct crypto_blkcipher *tfm;
 	struct s390_aes_ctx *sctx = crypto_blkcipher_ctx(desc->tfm);
+	struct blkcipher_desc fallback_desc = {
+		.tfm = sctx->fallback.blk,
+		.info = desc->info,
+		.flags = desc->flags
+	};
 
-	tfm = desc->tfm;
-	desc->tfm = sctx->fallback.blk;
-
-	ret = crypto_blkcipher_decrypt_iv(desc, dst, src, nbytes);
-
-	desc->tfm = tfm;
-	return ret;
+	return crypto_blkcipher_decrypt_iv(&fallback_desc, dst, src, nbytes);
 }
 
 static int fallback_blk_enc(struct blkcipher_desc *desc,
 		struct scatterlist *dst, struct scatterlist *src,
 		unsigned int nbytes)
 {
-	unsigned int ret;
-	struct crypto_blkcipher *tfm;
 	struct s390_aes_ctx *sctx = crypto_blkcipher_ctx(desc->tfm);
+	struct blkcipher_desc fallback_desc = {
+		.tfm = sctx->fallback.blk,
+		.info = desc->info,
+		.flags = desc->flags
+	};
 
-	tfm = desc->tfm;
-	desc->tfm = sctx->fallback.blk;
-
-	ret = crypto_blkcipher_encrypt_iv(desc, dst, src, nbytes);
-
-	desc->tfm = tfm;
-	return ret;
+	return crypto_blkcipher_encrypt_iv(&fallback_desc, dst, src, nbytes);
 }
 
 static int ecb_aes_set_key(struct crypto_tfm *tfm, const u8 *in_key,
@@ -472,16 +466,13 @@ static int xts_fallback_decrypt(struct blkcipher_desc *desc,
 		unsigned int nbytes)
 {
 	struct s390_xts_ctx *xts_ctx = crypto_blkcipher_ctx(desc->tfm);
-	struct crypto_blkcipher *tfm;
-	unsigned int ret;
+	struct blkcipher_desc fallback_desc = {
+		.tfm = xts_ctx->fallback,
+		.info = desc->info,
+		.flags = desc->flags
+	};
 
-	tfm = desc->tfm;
-	desc->tfm = xts_ctx->fallback;
-
-	ret = crypto_blkcipher_decrypt_iv(desc, dst, src, nbytes);
-
-	desc->tfm = tfm;
-	return ret;
+	return crypto_blkcipher_decrypt_iv(&fallback_desc, dst, src, nbytes);
 }
 
 static int xts_fallback_encrypt(struct blkcipher_desc *desc,
@@ -489,16 +480,13 @@ static int xts_fallback_encrypt(struct blkcipher_desc *desc,
 		unsigned int nbytes)
 {
 	struct s390_xts_ctx *xts_ctx = crypto_blkcipher_ctx(desc->tfm);
-	struct crypto_blkcipher *tfm;
-	unsigned int ret;
+	struct blkcipher_desc fallback_desc = {
+		.tfm = xts_ctx->fallback,
+		.info = desc->info,
+		.flags = desc->flags
+	};
 
-	tfm = desc->tfm;
-	desc->tfm = xts_ctx->fallback;
-
-	ret = crypto_blkcipher_encrypt_iv(desc, dst, src, nbytes);
-
-	desc->tfm = tfm;
-	return ret;
+	return crypto_blkcipher_encrypt_iv(&fallback_desc, dst, src, nbytes);
 }
 
 static int xts_aes_set_key(struct crypto_tfm *tfm, const u8 *in_key,
