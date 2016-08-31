8da21ed409dfab79cc5e3aef7a45bec18acd8ff7
Stefan Haberland <sth@linux.vnet.ibm.com>
2016-08-31 13:31:10 +0200

    s390/dasd: add missing KOBJ_CHANGE event for unformatted devices
    The DASD device driver throws change events for the DASD blockdevice
    after the online processing is done so that udev rules can take
    actions after it.
    The change event was missing for unformatted devices.
    
    Signed-off-by: Stefan Haberland <sth@linux.vnet.ibm.com>

--- drivers/s390/block/dasd.c
+++ drivers/s390/block/dasd.c
@@ -338,6 +338,7 @@ static int dasd_state_basic_to_ready(struct dasd_device *device)
 {
 	int rc;
 	struct dasd_block *block;
+	struct gendisk *disk;
 
 	rc = 0;
 	block = device->block;
@@ -348,6 +349,9 @@ static int dasd_state_basic_to_ready(struct dasd_device *device)
 		if (rc) {
 			if (rc != -EAGAIN) {
 				device->state = DASD_STATE_UNFMT;
+				disk = device->block->gdp;
+				kobject_uevent(&disk_to_dev(disk)->kobj,
+					       KOBJ_CHANGE);
 				goto out;
 			}
 			return rc;
