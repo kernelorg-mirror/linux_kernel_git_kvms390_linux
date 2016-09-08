63b4ee31b5b1bbd61d33f3066c611f21de4911e4
Janosch Frank <frankja@linux.vnet.ibm.com>
2016-09-08 09:53:48 +0200

    Revert "perf tools: Fix s390 building"
    Wrong way to fix the build. We would either also need to fix the bpf
    build test or change the build process.
    
    This reverts commit 74391f7b75f8d9c9610f08aa8b8efaaa0416698c.

--- tools/lib/bpf/bpf.c
+++ tools/lib/bpf/bpf.c
@@ -24,8 +24,6 @@
 #  define __NR_bpf 321
 # elif defined(__aarch64__)
 #  define __NR_bpf 280
-# elif defined(__s390__)
-#  define __NR_bpf 351
 # else
 #  error __NR_bpf not defined. libbpf does not support your arch.
 # endif
