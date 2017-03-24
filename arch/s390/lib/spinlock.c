82ec9a0aedcfbc0546cf6c0a0a763973492f1f76
Martin Schwidefsky <schwidefsky@de.ibm.com>
2017-03-24 17:14:41 +0100

    s390/spinlock: cpu yielding for non-head CPUs
    The idea that only the head CPU in the wait queue yields to the spinlock
    holder does not work. It can easily happend that the head CPU in the
    wait queue is preempted by the hypervisor while another CPU has the
    lock. If the lock is released the spinlock is blocked until the head CPU
    is scheduled again.
    
    Add a retry loop and a check for the running state of the spinlock holder
    or the head CPU of the wait queue for all CPUs in the wait queue.
    
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/lib/spinlock.c
+++ arch/s390/lib/spinlock.c
@@ -34,9 +34,9 @@ static int __init spin_retry_setup(char *str)
 __setup("spin_retry=", spin_retry_setup);
 
 struct spin_wait {
-	struct spin_wait *next;
-	int lock_spin;
-};
+	struct spin_wait *next, *prev;
+	int node_id;
+} __aligned(32);
 
 static DEFINE_PER_CPU_ALIGNED(struct spin_wait, spin_wait[4]);
 
@@ -53,17 +53,38 @@ static DEFINE_PER_CPU_ALIGNED(struct spin_wait, spin_wait[4]);
 #define _Q_LOCK_MASK		(_Q_LOCK_CPU_MASK | _Q_LOCK_STEAL_MASK)
 #define _Q_TAIL_MASK		(_Q_TAIL_IDX_MASK | _Q_TAIL_CPU_MASK)
 
+static inline struct spin_wait *arch_spin_decode_tail(int lock)
+{
+	int ix, cpu;
+
+	ix = (lock & _Q_TAIL_IDX_MASK) >> _Q_TAIL_IDX_OFFSET;
+	cpu = (lock & _Q_TAIL_CPU_MASK) >> _Q_TAIL_CPU_OFFSET;
+	return per_cpu_ptr(&spin_wait[ix], cpu - 1);
+}
+
+static inline int arch_spin_yield_target(int lock, struct spin_wait *node)
+{
+	if (lock & _Q_LOCK_CPU_MASK)
+		return lock & _Q_LOCK_CPU_MASK;
+	if (node->prev == NULL)
+		return 0;       /* 0 -> no target cpu */
+	while (node->prev)
+		node = node->prev;
+	return node->node_id >> _Q_TAIL_CPU_OFFSET;
+}
+
 void arch_spin_lock_wait(arch_spinlock_t *lp)
 {
-	struct spin_wait *node, *prev, *next;
-	int lockval, ix, cpu, node_id, tail_id, old, new, owner, count;
+	struct spin_wait *node, *next;
+	int lockval, ix, node_id, tail_id, old, new, owner, count;
 
 	ix = S390_lowcore.spinlock_index++;
 	barrier();
 	lockval = SPINLOCK_LOCKVAL;	/* cpu + 1 */
 	node = this_cpu_ptr(&spin_wait[ix]);
-	node_id = (lockval << _Q_TAIL_CPU_OFFSET) | (ix << _Q_TAIL_IDX_OFFSET);
-	memset(node, 0, sizeof(*node));
+	node->prev = node->next = NULL;
+	node->node_id = node_id =
+		(lockval << _Q_TAIL_CPU_OFFSET) | (ix << _Q_TAIL_IDX_OFFSET);
 
 	/* Enqueue the node for this CPU in the spinlock wait queue */
 	while (1) {
@@ -91,26 +112,26 @@ void arch_spin_lock_wait(arch_spinlock_t *lp)
 	/* Set the 'next' pointer of the tail node in the queue */
 	tail_id = old & _Q_TAIL_MASK;
 	if (tail_id != 0) {
-		ix = (tail_id & _Q_TAIL_IDX_MASK) >> _Q_TAIL_IDX_OFFSET;
-		cpu = (tail_id & _Q_TAIL_CPU_MASK) >> _Q_TAIL_CPU_OFFSET;
-		prev = per_cpu_ptr(&spin_wait[ix], cpu - 1);
-		WRITE_ONCE(prev->next, node);
+		node->prev = arch_spin_decode_tail(tail_id);
+		WRITE_ONCE(node->prev->next, node);
 	}
 
 	/* Pass the virtual CPU to the lock holder if it is not running */
-	owner = old & _Q_LOCK_CPU_MASK;
+	owner = arch_spin_yield_target(old, node);
 	if (owner && arch_vcpu_is_preempted(owner - 1))
 		smp_yield_cpu(owner - 1);
 
-	/* Spin on the CPU local 'lock_spin' word */
+	/* Spin on the CPU local node->prev pointer */
 	if (tail_id != 0) {
 		count = spin_retry;
-		while (1) {
-			if (READ_ONCE(node->lock_spin))
-				break;
+		while (READ_ONCE(node->prev) != NULL) {
 			if (count-- >= 0)
 				continue;
 			count = spin_retry;
+			/* Query running state of lock holder again. */
+			owner = arch_spin_yield_target(old, node);
+			if (owner && arch_vcpu_is_preempted(owner - 1))
+				smp_yield_cpu(owner - 1);
 		}
 	}
 
@@ -139,7 +160,7 @@ void arch_spin_lock_wait(arch_spinlock_t *lp)
 		/* Wait until the next CPU has set up the 'next' pointer */
 		while ((next = READ_ONCE(node->next)) == NULL)
 			;
-		next->lock_spin = 1;
+		next->prev = NULL;
 	}
 
  out:
