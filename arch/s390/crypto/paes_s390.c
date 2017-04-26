283b10083d695569100c7a2efd4e30121da142b4
Harald Freudenberger <freude@linux.vnet.ibm.com>
2017-04-26 15:46:07 +0200

    s390/crypt: Remove unneeded aes-all alias from paes kernel module.
    The paes module had an alias aes-all and thus was loaded by
    default when the kernel inserts the aes module(s). However,
    the paes module should be loaded only on request.
    This patch removes the aes-all alias from the module code.
    
    @Martin: Consider merging with
    	 "4bfca3f s390/crypt: Add module alias paes for paes_s390."
    
    Signed-off-by: Harald Freudenberger <freude@linux.vnet.ibm.com>

--- arch/s390/crypto/paes_s390.c
+++ arch/s390/crypto/paes_s390.c
@@ -617,7 +617,6 @@ module_init(paes_s390_init);
 module_exit(paes_s390_fini);
 
 MODULE_ALIAS_CRYPTO("paes");
-MODULE_ALIAS_CRYPTO("aes-all");
 
 MODULE_DESCRIPTION("Rijndael (AES) Cipher Algorithm with protected keys");
 MODULE_LICENSE("GPL");
