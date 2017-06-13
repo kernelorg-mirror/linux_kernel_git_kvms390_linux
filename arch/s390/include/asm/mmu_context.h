38c73ed78b36e9b1ceecffda744141d78ce42ea4
Martin Schwidefsky <schwidefsky@de.ibm.com>
2017-06-13 12:56:38 +0200

    s390/kvm: avoid global config of vm.alloc_pgste=1 #2
    Accidentally pushed the version with test_and_clear_thread_flag
    in init_new_context. Do not clear the TIF_PGSTE bit.
    
    To ber merged with git commit 3e489b157d9be36d4647f2b697652cdc4f6f1b11
    "s390/kvm: avoid global config of vm.alloc_pgste=1"
    
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/mmu_context.h
+++ arch/s390/include/asm/mmu_context.h
@@ -26,7 +26,7 @@ static inline int init_new_context(struct task_struct *tsk,
 	mm->context.flush_mm = 0;
 #ifdef CONFIG_PGSTE
 	mm->context.alloc_pgste = page_table_allocate_pgste ||
-		test_and_clear_thread_flag(TIF_PGSTE) ||
+		test_thread_flag(TIF_PGSTE) ||
 		current->mm->context.alloc_pgste;
 	mm->context.has_pgste = 0;
 	mm->context.use_skey = 0;
