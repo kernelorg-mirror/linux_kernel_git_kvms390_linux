664cfb5f11b0584cce61a30a7238b548e0816151
Vasily Gorbik <gor@linux.vnet.ibm.com>
2017-10-17 13:01:37 +0200

    s390/alternatives: add Kconfig option
    Add Kconfig option to enable/disable CPU alternatives. The option is
    enabled by default.
    
    When alternatives are disabled, no additional sections or patching code
    are present.
    
    ALTERNATIVES and SHARED_KERNEL options are mutually exclusive.
    
    Signed-off-by: Vasily Gorbik <gor@linux.vnet.ibm.com>

--- arch/s390/kernel/module.c
+++ arch/s390/kernel/module.c
@@ -431,14 +431,18 @@ int module_finalize(const Elf_Ehdr *hdr,
 		    struct module *me)
 {
 	const Elf_Shdr *s;
-	char *secstrings = (void *)hdr + sechdrs[hdr->e_shstrndx].sh_offset;
+	char *secstrings;
 
-	for (s = sechdrs; s < sechdrs + hdr->e_shnum; s++) {
-		if (!strcmp(".altinstructions", secstrings + s->sh_name)) {
-			/* patch .altinstructions */
-			void *aseg = (void *)s->sh_addr;
+	if (IS_ENABLED(CONFIG_ALTERNATIVES)) {
+		secstrings = (void *)hdr + sechdrs[hdr->e_shstrndx].sh_offset;
+		for (s = sechdrs; s < sechdrs + hdr->e_shnum; s++) {
+			if (!strcmp(".altinstructions",
+				    secstrings + s->sh_name)) {
+				/* patch .altinstructions */
+				void *aseg = (void *)s->sh_addr;
 
-			apply_alternatives(aseg, aseg + s->sh_size);
+				apply_alternatives(aseg, aseg + s->sh_size);
+			}
 		}
 	}
 
