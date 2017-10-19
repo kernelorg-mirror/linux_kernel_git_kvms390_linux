5f32a3d9c9ab8e789863de9b80e4a2183e8eb2f7
Martin Schwidefsky <schwidefsky@de.ibm.com>
2017-10-19 12:39:46 +0200

    s390/nmi: fix non-smp compile
    NMI now calls smp_emergency()_stop instead of smp_send_stop().
    This causes the following error for CONFIG_SMP=n:
    
    arch/s390/kernel/nmi.c: In function 's390_handle_damage':
    arch/s390/kernel/nmi.c:126:2: error: implicit declaration of function 'smp_emergency_stop'; did you mean 'emergency_sync'? [-Werror=implicit-function-declaration]
      smp_emergency_stop();
      ^~~~~~~~~~~~~~~~~~
      emergency_sync
    
    For smp_send_stop() the common header include/linux/smp.h
    defines an empty inline for CONFIG_SMP=n. For smp_emergency_stop
    the s390 header needs to do this.
    
    To be merged with git commit c6b8cf6add593888be0c9c7bebca751d9019c9a8
    "s390/nmi: use smp_emergency_stop instead of smp_send_stop"
    
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/smp.h
+++ arch/s390/include/asm/smp.h
@@ -27,6 +27,7 @@ extern void arch_send_call_function_ipi_mask(const struct cpumask *mask);
 
 extern void smp_call_online_cpu(void (*func)(void *), void *);
 extern void smp_call_ipl_cpu(void (*func)(void *), void *);
+extern void smp_emergency_stop(void);
 
 extern int smp_find_processor_id(u16 address);
 extern int smp_store_status(int cpu);
@@ -37,7 +38,6 @@ extern void smp_cpu_set_polarization(int cpu, int val);
 extern int smp_cpu_get_polarization(int cpu);
 extern void smp_fill_possible_mask(void);
 extern void smp_detect_cpus(void);
-extern void smp_emergency_stop(void);
 
 #else /* CONFIG_SMP */
 
@@ -51,6 +51,10 @@ static inline void smp_call_ipl_cpu(void (*func)(void *), void *data)
 static inline void smp_call_online_cpu(void (*func)(void *), void *data)
 {
 	func(data);
+}
+
+static void smp_emergency_stop(void)
+{
 }
 
 static inline int smp_find_processor_id(u16 address) { return 0; }
