a361fafb66faaa984b5275ec1d57115bd9b1db37
Martin Schwidefsky <schwidefsky@de.ibm.com>
2017-12-04 10:39:38 +0100

    s390: add a few more SPDX identifiers
    Add the correct SPDX license to a few more files under arch/s390 and
    drivers/s390 which have been missed to far.
    The SPDX identifier is a legally binding shorthand, which can be used
    instead of the full boiler plate text.
    
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/kernel/kmsg.c
+++ arch/s390/kernel/kmsg.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
  * Message printing with message catalog prefixes.
  *
--- drivers/s390/char/sclp_sd.c
+++ drivers/s390/char/sclp_sd.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
  * SCLP Store Data support and sysfs interface
  *
