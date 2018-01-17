4ae422ec9545ef5f9321a7134b143b9e70b6053e
Martin Schwidefsky <schwidefsky@de.ibm.com>
2018-01-17 08:46:22 +0100

    Remove whitespace
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- tools/arch/s390/include/uapi/asm/unistd.h
+++ tools/arch/s390/include/uapi/asm/unistd.h
@@ -319,7 +319,7 @@
 #define __NR_s390_sthyi		380
 #define NR_syscalls 381
 
-/* 
+/*
  * There are some system calls that are not present on 64 bit, some
  * have a different name although they do the same (e.g. __NR_chown32
  * is __NR_chown on 64 bit).
