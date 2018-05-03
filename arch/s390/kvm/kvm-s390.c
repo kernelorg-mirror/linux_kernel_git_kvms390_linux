91724eb629e2f5aac636ac313cf31cbcefb6aa41
Janosch Frank <frankja@linux.ibm.com>
2018-05-03 09:35:58 +0200

    fixup: KVM: s390: generalize kvm_s390_get_tod_clock_ext()
    We don't want to leak information via the kvm_s390_vm_tod_clock
    struct.
    
    Please merge with:
    29b4de3 KVM: s390: generalize kvm_s390_get_tod_clock_ext()
    
    Signed-off-by: Janosch Frank <frankja@linux.ibm.com>

--- arch/s390/kvm/kvm-s390.c
+++ arch/s390/kvm/kvm-s390.c
@@ -1064,6 +1064,7 @@ static int kvm_s390_get_tod_ext(struct kvm *kvm, struct kvm_device_attr *attr)
 {
 	struct kvm_s390_vm_tod_clock gtod;
 
+	memset(&gtod, 0, sizeof(gtod));
 	kvm_s390_get_tod_clock(kvm, &gtod);
 	if (copy_to_user((void __user *)attr->addr, &gtod, sizeof(gtod)))
 		return -EFAULT;
