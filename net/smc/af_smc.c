29a6c8da942d6688cca543b69882817991e02f2c
Ursula Braun <ursula.braun@de.ibm.com>
2018-07-06 14:10:19 +0200

    net/smc: remove sock locks surrounding sock_poll_wait
    leftover from the poll revert of Linus: adapt to the version in
    linux-next.
    
    Signed-off-by: Ursula Braun <ubraun@linux.ibm.com>

--- net/smc/af_smc.c
+++ net/smc/af_smc.c
@@ -1527,11 +1527,8 @@ static __poll_t smc_poll(struct file *file, struct socket *sock,
 		if (sk->sk_err)
 			mask |= EPOLLERR;
 	} else {
-		if (sk->sk_state != SMC_CLOSED) {
-			release_sock(sk);
+		if (sk->sk_state != SMC_CLOSED)
 			sock_poll_wait(file, sk_sleep(sk), wait);
-			lock_sock(sk);
-		}
 		if (sk->sk_err)
 			mask |= EPOLLERR;
 		if ((sk->sk_shutdown == SHUTDOWN_MASK) ||
