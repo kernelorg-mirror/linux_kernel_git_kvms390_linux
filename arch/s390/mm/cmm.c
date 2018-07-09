6b73fe085dc95f6b736739e2e6fa89be1b306518
Vasily Gorbik <gor@linux.ibm.com>
2018-07-09 13:21:30 +0200

    Revert "s390/cmm: use unsigned values for page counts and timeouts"
    Converting cmm code to unsigned long usage supposed to be an easy
    improvement but would require additional fixes on top to handle not
    atomic anymore cmm_timer_fn function and adding READ_ONCE/WRITE_ONCE,
    which would make sense together with entire cmm.c convertion to
    READ_ONCE/WRITE_ONCE usage (instead of inconsistent volatile usage).
    But that turned out to be a rather big mess with low benefit and a
    chance to break working code with bigger changes.
    
    So reverting commit 4c77b5677a41 for now.
    
    Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>

--- arch/s390/mm/cmm.c
+++ arch/s390/mm/cmm.c
@@ -43,12 +43,12 @@ struct cmm_page_array {
 	unsigned long pages[CMM_NR_PAGES];
 };
 
-static unsigned long cmm_pages;
-static unsigned long cmm_timed_pages;
-static volatile unsigned long cmm_pages_target;
-static volatile unsigned long cmm_timed_pages_target;
-static unsigned long cmm_timeout_pages;
-static unsigned long cmm_timeout_seconds;
+static long cmm_pages;
+static long cmm_timed_pages;
+static volatile long cmm_pages_target;
+static volatile long cmm_timed_pages_target;
+static long cmm_timeout_pages;
+static long cmm_timeout_seconds;
 static int cmm_suspended;
 
 static struct cmm_page_array *cmm_page_list;
@@ -62,8 +62,8 @@ static void cmm_timer_fn(struct timer_list *);
 static void cmm_set_timer(void);
 static DEFINE_TIMER(cmm_timer, cmm_timer_fn);
 
-static unsigned long cmm_alloc_pages(unsigned long nr, unsigned long *counter,
-				     struct cmm_page_array **list)
+static long cmm_alloc_pages(long nr, long *counter,
+			    struct cmm_page_array **list)
 {
 	struct cmm_page_array *pa, *npa;
 	unsigned long addr;
@@ -102,8 +102,7 @@ static unsigned long cmm_alloc_pages(unsigned long nr, unsigned long *counter,
 	return nr;
 }
 
-static unsigned long cmm_free_pages(unsigned long nr, unsigned long *counter,
-				    struct cmm_page_array **list)
+static long cmm_free_pages(long nr, long *counter, struct cmm_page_array **list)
 {
 	struct cmm_page_array *pa;
 	unsigned long addr;
@@ -131,7 +130,7 @@ static int cmm_oom_notify(struct notifier_block *self,
 			  unsigned long dummy, void *parm)
 {
 	unsigned long *freed = parm;
-	unsigned long nr = 256;
+	long nr = 256;
 
 	nr = cmm_free_pages(nr, &cmm_timed_pages, &cmm_timed_page_list);
 	if (nr > 0)
@@ -202,7 +201,7 @@ static void cmm_set_timer(void)
 
 static void cmm_timer_fn(struct timer_list *unused)
 {
-	unsigned long nr;
+	long nr;
 
 	nr = cmm_timed_pages_target - cmm_timeout_pages;
 	if (nr < 0)
@@ -213,29 +212,29 @@ static void cmm_timer_fn(struct timer_list *unused)
 	cmm_set_timer();
 }
 
-static void cmm_set_pages(unsigned long nr)
+static void cmm_set_pages(long nr)
 {
 	cmm_pages_target = nr;
 	cmm_kick_thread();
 }
 
-static unsigned long cmm_get_pages(void)
+static long cmm_get_pages(void)
 {
 	return cmm_pages;
 }
 
-static void cmm_add_timed_pages(unsigned long nr)
+static void cmm_add_timed_pages(long nr)
 {
 	cmm_timed_pages_target += nr;
 	cmm_kick_thread();
 }
 
-static unsigned long cmm_get_timed_pages(void)
+static long cmm_get_timed_pages(void)
 {
 	return cmm_timed_pages;
 }
 
-static void cmm_set_timeout(unsigned long nr, unsigned long seconds)
+static void cmm_set_timeout(long nr, long seconds)
 {
 	cmm_timeout_pages = nr;
 	cmm_timeout_seconds = seconds;
@@ -255,11 +254,11 @@ static int cmm_skip_blanks(char *cp, char **endp)
 static int cmm_pages_handler(struct ctl_table *ctl, int write,
 			     void __user *buffer, size_t *lenp, loff_t *ppos)
 {
-	unsigned long nr = cmm_get_pages();
+	long nr = cmm_get_pages();
 	struct ctl_table ctl_entry = {
 		.procname	= ctl->procname,
 		.data		= &nr,
-		.maxlen		= sizeof(unsigned long),
+		.maxlen		= sizeof(long),
 	};
 	int rc;
 
@@ -275,11 +274,11 @@ static int cmm_timed_pages_handler(struct ctl_table *ctl, int write,
 				   void __user *buffer, size_t *lenp,
 				   loff_t *ppos)
 {
-	unsigned long nr = cmm_get_timed_pages();
+	long nr = cmm_get_timed_pages();
 	struct ctl_table ctl_entry = {
 		.procname	= ctl->procname,
 		.data		= &nr,
-		.maxlen		= sizeof(unsigned long),
+		.maxlen		= sizeof(long),
 	};
 	int rc;
 
@@ -295,7 +294,7 @@ static int cmm_timeout_handler(struct ctl_table *ctl, int write,
 			       void __user *buffer, size_t *lenp, loff_t *ppos)
 {
 	char buf[64], *p;
-	unsigned long nr, seconds;
+	long nr, seconds;
 	unsigned int len;
 
 	if (!*lenp || (*ppos && !write)) {
@@ -360,7 +359,7 @@ static struct ctl_table cmm_dir_table[] = {
 #define SMSG_PREFIX "CMM"
 static void cmm_smsg_target(const char *from, char *msg)
 {
-	unsigned long nr, seconds;
+	long nr, seconds;
 
 	if (strlen(sender) > 0 && strcmp(from, sender) != 0)
 		return;
