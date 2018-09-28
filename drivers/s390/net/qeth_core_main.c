0b6c0d3e0a3807d5d1cf79098e0d321a30607a6f
Julian Wiedmann <jwi@linux.ibm.com>
2018-09-28 10:39:24 +0200

    s390/qeth: replace open-coded cmd setup
    Use qeth_prepare_ipa_cmd() for initializing ARP and SNMP requests. This
    will also set the proper MPC protocol version for SNMP requests on
    L2 devices.
    
    Signed-off-by: Julian Wiedmann <jwi@linux.ibm.com>

--- drivers/s390/net/qeth_core_main.c
+++ drivers/s390/net/qeth_core_main.c
@@ -4505,9 +4505,7 @@ static int qeth_send_ipa_snmp_cmd(struct qeth_card *card,
 
 	QETH_CARD_TEXT(card, 4, "sendsnmp");
 
-	memcpy(iob->data, IPA_PDU_HEADER, IPA_PDU_HEADER_SIZE);
-	memcpy(QETH_IPA_CMD_DEST_ADDR(iob->data),
-	       &card->token.ulp_connection_r, QETH_MPC_TOKEN_LENGTH);
+	qeth_prepare_ipa_cmd(card, iob);
 	/* adjust PDU length fields in IPA_PDU_HEADER */
 	s1 = (u32) IPA_PDU_HEADER_SIZE + len;
 	s2 = (u32) len;
--- drivers/s390/net/qeth_l3_main.c
+++ drivers/s390/net/qeth_l3_main.c
@@ -1703,9 +1703,7 @@ static int qeth_l3_send_ipa_arp_cmd(struct qeth_card *card,
 {
 	QETH_CARD_TEXT(card, 4, "sendarp");
 
-	memcpy(iob->data, IPA_PDU_HEADER, IPA_PDU_HEADER_SIZE);
-	memcpy(QETH_IPA_CMD_DEST_ADDR(iob->data),
-	       &card->token.ulp_connection_r, QETH_MPC_TOKEN_LENGTH);
+	qeth_prepare_ipa_cmd(card, iob);
 	return qeth_send_control_data(card, IPA_PDU_HEADER_SIZE + len, iob,
 				      reply_cb, reply_param);
 }
