9a36ab8cd4423efdaa81dfd36f8e9b20c7574538
Martin Schwidefsky <schwidefsky@de.ibm.com>
2019-01-22 09:39:25 +0100

    objtool: remove __COUNTER__ based labels
    The use of "asm volatile" is enough to convince gcc not to merge
    inline assemblies. The use of __COUNTER__ is superfluous and it
    leads to buggy expansions like this on s390:
    
            -78:
            .pushsection .discard.unreachable
            .long -78b - .
            .popsection
    
    The assembler will barf on the negative label number.
    
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- include/linux/compiler.h
+++ include/linux/compiler.h
@@ -99,16 +99,16 @@ void ftrace_likely_update(struct ftrace_likely_data *f, int val,
  * unique, to convince GCC not to merge duplicate inline asm statements.
  */
 #define annotate_reachable() ({						\
-	asm volatile("%c0:\n\t"						\
+	asm volatile("1:\n\t"						\
 		     ".pushsection .discard.reachable\n\t"		\
-		     ".long %c0b - .\n\t"				\
-		     ".popsection\n\t" : : "i" (__COUNTER__));		\
+		     ".long 1b - .\n\t"					\
+		     ".popsection\n\t");				\
 })
 #define annotate_unreachable() ({					\
-	asm volatile("%c0:\n\t"						\
+	asm volatile("1:\n\t"						\
 		     ".pushsection .discard.unreachable\n\t"		\
-		     ".long %c0b - .\n\t"				\
-		     ".popsection\n\t" : : "i" (__COUNTER__));		\
+		     ".long 1b - .\n\t"					\
+		     ".popsection\n\t");				\
 })
 #define ASM_UNREACHABLE							\
 	"999:\n\t"							\
