b1febb931e8bddb3ca8c4f09604e8024d96eac1c
Heiko Carstens <heiko.carstens@de.ibm.com>
2019-05-01 15:54:10 +0200

    Revert "objtool: fix dis-defs.h include with build directory"
    This reverts commit 727d148c60e8f70a4c2b354524ef441ccd673cb9.

---
+++ tools/objtool/arch/s390/include/asm/dis-defs.h
@@ -0,0 +1,6 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright IBM Coprt. 2019
+ * Author(s): Martin Schwidefsky <schwidefsky@de.ibm.com>
+ */
+#include <../../../../../arch/s390/include/generated/asm/dis-defs.h>
