aee03b89d519442fe3d951ad6d9e4cbf3e79f7ca
Heiko Carstens <heiko.carstens@de.ibm.com>
2019-05-01 15:54:55 +0200

    Revert "s390: enable stack validation support"
    This reverts commit 83a420894ab2c952f588409d886ac484e393e9f6.

--- arch/s390/kernel/setup.c
+++ arch/s390/kernel/setup.c
@@ -49,7 +49,6 @@
 #include <linux/memory.h>
 #include <linux/compat.h>
 #include <linux/start_kernel.h>
-#include <linux/frame.h>
 
 #include <asm/boot_data.h>
 #include <asm/ipl.h>
@@ -385,7 +384,6 @@ void __init arch_call_rest_init(void)
 		"	jg	rest_init\n"
 		: : [_frame] "a" (frame));
 }
-STACK_FRAME_NON_STANDARD(arch_call_rest_init);
 
 static void __init setup_lowcore_dat_off(void)
 {
