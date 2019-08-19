3a7000ca2e2ab92f77244e4a7606753065d210ae
Vasily Gorbik <gor@linux.ibm.com>
2019-08-19 10:49:53 +0200

    s390/startup: correct command line options parsing
    Check val is not NULL before accessing it. This might happen if
    corresponding kernel command line options are used without specifying
    values.
    
    Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>

--- arch/s390/boot/ipl_parm.c
+++ arch/s390/boot/ipl_parm.c
@@ -221,12 +221,12 @@ void parse_boot_command_line(void)
 	while (*args) {
 		args = next_arg(args, &param, &val);
 
-		if (!strcmp(param, "mem")) {
+		if (!strcmp(param, "mem") && val) {
 			memory_end = round_down(memparse(val, NULL), PAGE_SIZE);
 			memory_end_set = 1;
 		}
 
-		if (!strcmp(param, "vmalloc"))
+		if (!strcmp(param, "vmalloc") && val)
 			vmalloc_size = round_up(memparse(val, NULL), PAGE_SIZE);
 
 		if (!strcmp(param, "noexec")) {
@@ -235,7 +235,7 @@ void parse_boot_command_line(void)
 				noexec_disabled = 1;
 		}
 
-		if (!strcmp(param, "facilities"))
+		if (!strcmp(param, "facilities") && val)
 			modify_fac_list(val);
 
 		if (!strcmp(param, "nokaslr"))
