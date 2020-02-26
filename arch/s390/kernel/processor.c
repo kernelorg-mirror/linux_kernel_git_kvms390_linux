c51f3b7b44aa50535ccfe13819bc6a5959a624fa
Heiko Carstens <heiko.carstens@de.ibm.com>
2020-02-26 12:07:54 +0100

    s390: fix allnoconfig build
    please merge with 2003-s390-cpuinfo-add-system-topology-information.patch
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>

--- arch/s390/kernel/processor.c
+++ arch/s390/kernel/processor.c
@@ -151,13 +151,15 @@ static void show_cpu_summary(struct seq_file *m, void *v)
 	}
 }
 
-static void show_cpu_topo(struct seq_file *m, unsigned long n)
+static void show_cpu_topology(struct seq_file *m, unsigned long n)
 {
+#ifdef CONFIG_SCHED_TOPOLOGY
 	seq_printf(m, "physical id     : %d\n", topology_physical_package_id(n));
 	seq_printf(m, "core id         : %d\n", topology_core_id(n));
 	seq_printf(m, "book id         : %d\n", topology_book_id(n));
 	seq_printf(m, "drawer id       : %d\n", topology_drawer_id(n));
 	seq_printf(m, "dedicated       : %d\n", topology_cpu_dedicated(n));
+#endif /* CONFIG_SCHED_TOPOLOGY */
 }
 
 static void show_cpu_ids(struct seq_file *m, unsigned long n)
@@ -189,7 +191,7 @@ static int show_cpuinfo(struct seq_file *m, void *v)
 	if (!machine_has_cpu_mhz)
 		return 0;
 	seq_printf(m, "\ncpu number      : %ld\n", n);
-	show_cpu_topo(m, n);
+	show_cpu_topology(m, n);
 	show_cpu_ids(m, n);
 	show_cpu_mhz(m, n);
 	return 0;
