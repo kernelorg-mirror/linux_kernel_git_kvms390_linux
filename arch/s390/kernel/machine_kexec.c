4e0cf8881812eb2aa39add7f5104719d8944c87e
Heiko Carstens <heiko.carstens@de.ibm.com>
2020-03-10 14:40:58 +0100

    Revert "s390/kexec: Select diag308 subcode depending on preserve_context"
    Revert 2001-s390-kexec-Select-diag308-subcode-depending-on-prese.patch
    as discussed with Philipp - not needed anymore.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>

--- arch/s390/kernel/machine_kexec.c
+++ arch/s390/kernel/machine_kexec.c
@@ -31,7 +31,7 @@
 #include <asm/switch_to.h>
 #include <asm/nmi.h>
 
-typedef void (*relocate_kernel_t)(kimage_entry_t *, unsigned long, unsigned long);
+typedef void (*relocate_kernel_t)(kimage_entry_t *, unsigned long);
 
 extern const unsigned char relocate_kernel[];
 extern const unsigned long long relocate_kernel_len;
@@ -284,8 +284,7 @@ static void __do_machine_kexec(void *data)
 
 	__arch_local_irq_stnsm(0xfb); /* disable DAT - avoid no-execute */
 	/* Call the moving routine */
-	(*data_mover)(&image->head, image->start,
-		      image->preserve_context ? DIAG308_MOD_CLR_RST : DIAG308_LD_NRM_RST);
+	(*data_mover)(&image->head, image->start);
 
 	/* Die if kexec returns */
 	disabled_wait();
