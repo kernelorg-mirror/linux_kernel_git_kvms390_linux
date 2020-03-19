0802f72e32098edc14df5bf57eb9224630af558e
Alexander Gordeev <agordeev@linux.ibm.com>
2020-03-19 13:44:48 +0100

    s390/mm: remove 2-level page-tables handling code
    Commit 8bb7244f34c6 ("s390/mm: remove page table downgrade support")
    made dead code paths that handle 2-level page-tables.
    
    Please merge with 2001-s390-mm-remove-page-table-downgrade-support.patch
    
    Reported-by: Gerald Schaefer <gerald.schaefer@de.ibm.com>
    Signed-off-by: Alexander Gordeev <agordeev@linux.ibm.com>
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>

--- arch/s390/include/asm/pgalloc.h
+++ arch/s390/include/asm/pgalloc.h
@@ -115,24 +115,11 @@ static inline void pud_populate(struct mm_struct *mm, pud_t *pud, pmd_t *pmd)
 
 static inline pgd_t *pgd_alloc(struct mm_struct *mm)
 {
-	unsigned long *table = crst_table_alloc(mm);
-
-	if (!table)
-		return NULL;
-	if (mm->context.asce_limit == _REGION3_SIZE) {
-		/* Forking a compat process with 2 page table levels */
-		if (!pgtable_pmd_page_ctor(virt_to_page(table))) {
-			crst_table_free(mm, table);
-			return NULL;
-		}
-	}
-	return (pgd_t *) table;
+	return (pgd_t *) crst_table_alloc(mm);
 }
 
 static inline void pgd_free(struct mm_struct *mm, pgd_t *pgd)
 {
-	if (mm->context.asce_limit == _REGION3_SIZE)
-		pgtable_pmd_page_dtor(virt_to_page(pgd));
 	crst_table_free(mm, (unsigned long *) pgd);
 }
 
