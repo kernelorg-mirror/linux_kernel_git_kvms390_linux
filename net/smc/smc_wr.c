577be83b7a2c79b9c976b43cc7060dc412f66852
Karsten Graul <kgraul@linux.ibm.com>
2020-05-28 14:10:16 +0200

    net/smc: improve work request handling
    Wait for pending sends only when smc_switch_conns() found a link to move
    the connections to. Do not wait during link freeing, and refuse to
    provide a new tx slot on an unusable link.
    
    Signed-off-by: Karsten Graul <kgraul@linux.ibm.com>

--- net/smc/smc_wr.c
+++ net/smc/smc_wr.c
@@ -169,6 +169,8 @@ void smc_wr_tx_cq_handler(struct ib_cq *ib_cq, void *cq_context)
 static inline int smc_wr_tx_get_free_slot_index(struct smc_link *link, u32 *idx)
 {
 	*idx = link->wr_tx_cnt;
+	if (!smc_link_usable(link))
+		return -ENOLINK;
 	for_each_clear_bit(*idx, link->wr_tx_mask, link->wr_tx_cnt) {
 		if (!test_and_set_bit(*idx, link->wr_tx_mask))
 			return 0;
@@ -559,11 +561,6 @@ static void smc_wr_init_sge(struct smc_link *lnk)
 void smc_wr_free_link(struct smc_link *lnk)
 {
 	struct ib_device *ibdev;
-
-	if (smc_wr_tx_wait_no_pending_sends(lnk))
-		memset(lnk->wr_tx_mask, 0,
-		       BITS_TO_LONGS(SMC_WR_BUF_CNT) *
-						sizeof(*lnk->wr_tx_mask));
 
 	if (!lnk->smcibdev)
 		return;
