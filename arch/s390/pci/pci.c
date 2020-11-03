a3d76812ab06372e89ec851b395b3b837cfaf7fe
Niklas Schnelle <schnelle@linux.ibm.com>
2020-11-03 09:23:39 +0100

    Revert "s390/pci: add warning when missing facilities"
    This reverts commit d3e0d5ff12337003a1dc71f5f35f4a5a3a660c51.

--- arch/s390/pci/pci.c
+++ arch/s390/pci/pci.c
@@ -851,10 +851,8 @@ static int __init pci_base_init(void)
 	if (!s390_pci_probe)
 		return 0;
 
-	if (!test_facility(69) || !test_facility(71)) {
-		pr_warn("PCI is not supported because CPU facilities 69 and 71 are not available\n");
+	if (!test_facility(69) || !test_facility(71))
 		return 0;
-	}
 
 	if (test_facility(153) && !s390_pci_no_mio) {
 		static_branch_enable(&have_mio);
