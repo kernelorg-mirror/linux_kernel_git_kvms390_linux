32956c355023f01aeb1e7d16f80f78c683585e0b
Guvenc Gulce <guvenc@linux.ibm.com>
2020-11-16 13:50:19 +0100

    net/smc: Add SMC-D Linkgroup diagnostic support
    Deliver SMCD Linkgroup information via netlink based
    diagnostic interface.
    
    Signed-off-by: Guvenc Gulce <guvenc@linux.ibm.com>

--- net/smc/smc_netlink.c
+++ net/smc/smc_netlink.c
@@ -24,6 +24,7 @@ static const struct nla_policy smc_gen_nl_policy[SMC_GEN_MAX + 1] = {
 	[SMC_GEN_SYS_INFO]	= { .type = NLA_NESTED, },
 	[SMC_GEN_LGR_SMCR]	= { .type = NLA_NESTED, },
 	[SMC_GEN_LINK_SMCR]	= { .type = NLA_NESTED, },
+	[SMC_GEN_LGR_SMCD]	= { .type = NLA_NESTED, },
 };
 
 static int smc_nl_start(struct netlink_callback *cb)
@@ -31,6 +32,7 @@ static int smc_nl_start(struct netlink_callback *cb)
 	struct smc_nl_dmp_ctx *cb_ctx = smc_nl_dmp_ctx(cb);
 
 	cb_ctx->pos[0] = 0;
+	cb_ctx->pos[1] = 0;
 	return 0;
 }
 
@@ -52,6 +54,12 @@ static const struct genl_ops smc_gen_nl_ops[] = {
 		.cmd = SMC_NETLINK_GET_LINK_SMCR,
 		/* can be retrieved by unprivileged users */
 		.dumpit = smcr_nl_get_link,
+		.start = smc_nl_start
+	},
+	{
+		.cmd = SMC_NETLINK_GET_LGR_SMCD,
+		/* can be retrieved by unprivileged users */
+		.dumpit = smcd_nl_get_lgr,
 		.start = smc_nl_start
 	},
 };
