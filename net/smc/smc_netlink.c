53e657fdd5212f51e6199a097e5dc5b58476f2b9
Guvenc Gulce <guvenc@linux.ibm.com>
2020-12-02 18:58:10 +0100

    Revert "net/smc: Add support for obtaining SMCD device list"
    This reverts commit 041381c33e13c81e5069ae65d14177064ff58c37.

--- net/smc/smc_netlink.c
+++ net/smc/smc_netlink.c
@@ -17,7 +17,6 @@
 #include <linux/smc.h>
 
 #include "smc_core.h"
-#include "smc_ism.h"
 #include "smc_netlink.h"
 
 static const struct nla_policy smc_gen_nl_policy[SMC_GEN_MAX + 1] = {
@@ -26,7 +25,6 @@ static const struct nla_policy smc_gen_nl_policy[SMC_GEN_MAX + 1] = {
 	[SMC_GEN_LGR_SMCR]	= { .type = NLA_NESTED, },
 	[SMC_GEN_LINK_SMCR]	= { .type = NLA_NESTED, },
 	[SMC_GEN_LGR_SMCD]	= { .type = NLA_NESTED, },
-	[SMC_GEN_DEV_SMCD]	= { .type = NLA_NESTED, },
 };
 
 static int smc_nl_start(struct netlink_callback *cb)
@@ -62,12 +60,6 @@ static const struct genl_ops smc_gen_nl_ops[] = {
 		.cmd = SMC_NETLINK_GET_LGR_SMCD,
 		/* can be retrieved by unprivileged users */
 		.dumpit = smcd_nl_get_lgr,
-		.start = smc_nl_start
-	},
-	{
-		.cmd = SMC_NETLINK_GET_DEV_SMCD,
-		/* can be retrieved by unprivileged users */
-		.dumpit = smcd_nl_get_device,
 		.start = smc_nl_start
 	},
 };
