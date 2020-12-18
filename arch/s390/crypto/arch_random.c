6ccbff21cf57d6bcf88870b39db47ea3c8ad9feb
Heiko Carstens <hca@linux.ibm.com>
2020-12-18 14:08:08 +0100

    Revert "s390/crypto: export missing symbol s390_arch_random_available"
    This reverts commit 8688a3b1772e76300a175ec6c692e8d1d24541f5.
    
    Kernel builds with allmodconfig. Not clear why this is necessary and I
    don't want to carry this in devel until this has been clarified.
    
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>

--- arch/s390/crypto/arch_random.c
+++ arch/s390/crypto/arch_random.c
@@ -37,7 +37,6 @@
 #include <asm/cpacf.h>
 
 DEFINE_STATIC_KEY_FALSE(s390_arch_random_available);
-EXPORT_SYMBOL(s390_arch_random_available);
 
 atomic64_t s390_arch_random_counter = ATOMIC64_INIT(0);
 EXPORT_SYMBOL(s390_arch_random_counter);
