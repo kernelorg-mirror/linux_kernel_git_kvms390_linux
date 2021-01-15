608246acbce7e47fafb73ab8082c05abc6694949
Sven Schnelle <svens@linux.ibm.com>
2021-01-15 20:26:36 +0100

    test: revert 3a630178fd5f ("tracing: generate RCU warnings even when tracepoints are disabled")
    On my lpar, the strace testsuite takes about 25 minutes with that patch,
    and without 15 minutes. Lets see what the impact on CI is.
    
    Will be reverted after the next CI run.
    
    Signed-off-by: Sven Schnelle <svens@linux.ibm.com>

--- include/linux/tracepoint.h
+++ include/linux/tracepoint.h
@@ -224,12 +224,6 @@ static inline struct tracepoint *tracepoint_ptr_deref(tracepoint_ptr_t *p)
  * not add unwanted padding between the beginning of the section and the
  * structure. Force alignment to the same alignment as the section start.
  *
- * When lockdep is enabled, we make sure to always do the RCU portions of
- * the tracepoint code, regardless of whether tracing is on. However,
- * don't check if the condition is false, due to interaction with idle
- * instrumentation. This lets us find RCU issues triggered with tracepoints
- * even when this tracepoint is off. This code has no purpose other than
- * poking RCU a bit.
  */
 #define __DECLARE_TRACE(name, proto, args, cond, data_proto, data_args) \
 	extern int __traceiter_##name(data_proto);			\
@@ -242,11 +236,6 @@ static inline struct tracepoint *tracepoint_ptr_deref(tracepoint_ptr_t *p)
 				TP_PROTO(data_proto),			\
 				TP_ARGS(data_args),			\
 				TP_CONDITION(cond), 0);			\
-		if (IS_ENABLED(CONFIG_LOCKDEP) && (cond)) {		\
-			rcu_read_lock_sched_notrace();			\
-			rcu_dereference_sched(__tracepoint_##name.funcs);\
-			rcu_read_unlock_sched_notrace();		\
-		}							\
 	}								\
 	__DECLARE_TRACE_RCU(name, PARAMS(proto), PARAMS(args),		\
 		PARAMS(cond), PARAMS(data_proto), PARAMS(data_args))	\
