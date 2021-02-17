192256e521d57d484698fba96cfe96e49b4bdb5a
Heiko Carstens <hca@linux.ibm.com>
2021-02-17 07:19:09 +0100

    s390/smp: smp_emergency_stop() - move cpumask away from stack
    Make "cpumask_t cpumask" a static variable to avoid a potential large
    stack frame. Also protect against potential concurrent callers by
    introducing a local lock.
    Note: smp_emergency_stop() gets only called with irqs and machine
    checks disabled, therefore a cpu local deadlock is not possible. For
    concurrent callers the first cpu which enters the critical section
    wins and will stop all other cpus.
    
    Reference-ID: KRN1936
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>

--- arch/s390/kernel/smp.c
+++ arch/s390/kernel/smp.c
@@ -435,10 +435,12 @@ EXPORT_SYMBOL(smp_yield_cpu);
  */
 void notrace smp_emergency_stop(void)
 {
-	cpumask_t cpumask;
+	static arch_spinlock_t lock = __ARCH_SPIN_LOCK_UNLOCKED;
+	static cpumask_t cpumask;
 	u64 end;
 	int cpu;
 
+	arch_spin_lock(&lock);
 	cpumask_copy(&cpumask, cpu_online_mask);
 	cpumask_clear_cpu(smp_processor_id(), &cpumask);
 
@@ -459,6 +461,7 @@ void notrace smp_emergency_stop(void)
 			break;
 		cpu_relax();
 	}
+	arch_spin_unlock(&lock);
 }
 NOKPROBE_SYMBOL(smp_emergency_stop);
 
