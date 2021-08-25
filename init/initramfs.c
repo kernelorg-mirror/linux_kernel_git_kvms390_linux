c4ba3f4e147bbf9649284957da72bb5998a37d35
Alexander Egorenkov <egorenar@linux.ibm.com>
2021-08-25 08:51:22 +0200

    Revert "fixup! s390/initrd: instrument decompressor/kernel to investigate initrd corruption"
    This reverts commit 83f7682ba3aa71e328608527e6d4e048a1894aff.

--- init/initramfs.c
+++ init/initramfs.c
@@ -686,16 +686,14 @@ static void __init do_populate_rootfs(void *unused, async_cookie_t cookie)
 {
 	char *err;
 
-	if (initrd_start) {
-		pr_info("%s:%d: initrd: 0x%016lx 0x%016lx\n",
-			__func__, __LINE__,
-			initrd_start,
-			initrd_end);
-		pr_info("%s:%d: initrd data: 0x%016lx 0x%016lx\n",
-			__func__, __LINE__,
-			((unsigned long *)initrd_start)[0],
-			((unsigned long *)initrd_end)[-1]);
-	}
+	pr_info("%s:%d: initrd: 0x%016lx 0x%016lx\n",
+		__func__, __LINE__,
+		initrd_start,
+		initrd_end);
+	pr_info("%s:%d: initrd data: 0x%016lx 0x%016lx\n",
+		__func__, __LINE__,
+		((unsigned long *)initrd_start)[0],
+		((unsigned long *)initrd_end)[-1]);
 
 	/* Load the built in initramfs */
 	err = unpack_to_rootfs(__initramfs_start, __initramfs_size);
