e641188aa022b7772d92a186e1bc8ffb04c0ec0f
Marco Elver <elver@google.com>
2021-08-31 16:00:03 +0200

    s390/stacktrace: do not include arch_stack_walk() in stack trace
    Callers of stack_trace_save() expect that it does not include itself,
    which attempts to exclude itself by skipping + 1. This contract is
    broken if arch_stack_walk() still includes itself.
    
    Fix it by skipping the initial entry in s390's arch_stack_walk().
    
    Signed-off-by: Marco Elver <elver@google.com>
    Link: https://lore.kernel.org/lkml/YS5Ed4Cyf+rOf9MR@elver.google.com
    [hca@linux.ibm.com: see also commit 0c32706dac1b ("arm64: stacktrace:
     avoid tracing arch_stack_walk()") for more information about this]
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>

--- arch/s390/kernel/stacktrace.c
+++ arch/s390/kernel/stacktrace.c
@@ -16,11 +16,15 @@ void arch_stack_walk(stack_trace_consume_fn consume_entry, void *cookie,
 {
 	struct unwind_state state;
 	unsigned long addr;
+	bool init = true;
 
 	unwind_for_each_frame(&state, task, regs, 0) {
 		addr = unwind_get_return_address(&state);
-		if (!addr || !consume_entry(cookie, addr))
+		if (!addr)
 			break;
+		if (!init && !consume_entry(cookie, addr))
+			break;
+		init = false;
 	}
 }
 
@@ -29,6 +33,7 @@ int arch_stack_walk_reliable(stack_trace_consume_fn consume_entry,
 {
 	struct unwind_state state;
 	unsigned long addr;
+	bool init = true;
 
 	unwind_for_each_frame(&state, task, NULL, 0) {
 		if (state.stack_info.type != STACK_TYPE_TASK)
@@ -50,8 +55,9 @@ int arch_stack_walk_reliable(stack_trace_consume_fn consume_entry,
 			return -EINVAL;
 #endif
 
-		if (!consume_entry(cookie, addr))
+		if (!init && !consume_entry(cookie, addr))
 			return -EINVAL;
+		init = false;
 	}
 
 	/* Check for stack corruption */
