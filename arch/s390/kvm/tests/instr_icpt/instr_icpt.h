63cb461a5185fa6b5635e3a5fb08b331964e1234
Nico Boehr <nrb@linux.ibm.com>
2022-05-31 16:10:16 +0200

    s390/kvm/tests: remove sck test
    The sck test was migrated to kvm-unit-tests with adf6cc0c2943 ("s390x:
    Add sck tests").
    
    Signed-off-by: Nico Boehr <nrb@linux.ibm.com>

--- arch/s390/kvm/tests/instr_icpt/instr_icpt.h
+++ arch/s390/kvm/tests/instr_icpt/instr_icpt.h
@@ -1,7 +1,6 @@
 
 void ztst_set_err_str(const char *str);
 
-int test_sck(void);
 int test_tb(void);
 int test_tprot(void);
 int test_sigp(void);
--- arch/s390/kvm/tests/instr_icpt/main.c
+++ arch/s390/kvm/tests/instr_icpt/main.c
@@ -10,7 +10,6 @@ static struct {
 	const char *name;
 	int (*func)(void);
 } tests[] = {
-	{ "sck", test_sck },
 	{ "tb", test_tb },
 	{ "tprot", test_tprot },
 	{ "stsi", test_stsi },
