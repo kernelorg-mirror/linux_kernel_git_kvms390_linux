5436ae8893a0c6ea8eeff453d4bc5600984ea723
Steffen Eiden <seiden@linux.ibm.com>
2026-04-02 06:00:00 +0200

    KVM: s390: Add basic arm64 kvm module
    Add basic code for the new arm64 on s390 KVM implementation.
    Add kernel module boilerplate code and trivial functions.
    Add tracing scaffolding.
    
    Signed-off-by: Steffen Eiden <seiden@linux.ibm.com>

---
+++ arch/s390/kvm/arm64/trace.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#if !defined(KVM_ARM64_TRACE_KVM_H) || defined(TRACE_HEADER_MULTI_READ)
+#define KVM_ARM64_TRACE_KVM_H
+
+#include <linux/tracepoint.h>
+
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM kvm
+
+#endif /* KVM_ARM64_TRACE_KVM_H */
+
+#undef TRACE_INCLUDE_PATH
+#define TRACE_INCLUDE_PATH .
+#undef TRACE_INCLUDE_FILE
+#define TRACE_INCLUDE_FILE trace
+
+/* This part must be outside protection */
+#include <trace/define_trace.h>
